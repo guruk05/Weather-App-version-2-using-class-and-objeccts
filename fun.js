7f4b264da0a67c9e067d46acf39f0c12574c5af0
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-14 22:17:17 +0530

    Initializing openweather api using fetch

--- fun.js
+++ fun.js
@@ -6,6 +6,6 @@ class Weather{
     }
     getWeather = () => {
         this.city = document.getElementById('cityName').value;
-        
-    }
-}
+
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key) 
+   
