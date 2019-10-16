7aedd2da8cc2023fec86674fd1996db383dfc800
guruk05 <54667515+guruk05@users.noreply.github.com>
2019-10-16 10:44:17 +0530

    added icon functionality

--- fun.js
+++ fun.js
@@ -6,9 +6,10 @@ class Weather{
         cityName = $( "#cityName" ).val();
         fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key) 
         .then((resp) => { return resp.json()
-        })                                                           // Convert data to json
+        })                                                                 // Convert data to json
         .then((data) => {
             weatherData.showWeather(data);
+          console.log(data);
         })
         .catch(() => {
         });
@@ -18,11 +19,16 @@ class Weather{
         let fahrenheit = Math.round(((parseFloat(data.main.temp) - 273.15) * 1.8) + 32);
         let description = data.weather[0].description;
         let country = data.sys.country;
+        let iconcode = data.weather[0].icon;
+        let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+        // console.log(iconurl);
         
         $('#showDescription').html(description);
         $('#showDegree').html(celsius);
         $('#showCity').html(data.name);
         $('#showCountry').html(country);
+        // $('#weatherIcon').src(iconurl);
+        // document.getElementById('weatherIcon').src = iconurl;
 
         $('.Celsius').on('click',() => {
             $('#showDegree').html(fahrenheit);
@@ -38,6 +44,7 @@ class Weather{
 
   
 let weatherData = new Weather();
+
 
 
   
